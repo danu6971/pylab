e692a51cb6d089137cd68447004e035bd2caea55
danu6971 <imdanu6971@gmail.com>
2025-04-14 13:18:21 +0530

    Add files via upload

---
+++ Lab5/lab5q1.py
@@ -0,0 +1,6 @@
+def remove_char(s,char):
+    return s.replace(char,'')
+string="hello"
+char_to_remove="l"
+result=remove_char(string,char_to_remove)
+print(result)
---
+++ Lab5/lab5q2.py
@@ -0,0 +1,6 @@
+def count_char_occurrences(s,char):
+    return s.count(char)
+string="hello"
+char_to_count="l"
+result=count_char_occurrences(string,char_to_count)
+print(result)
