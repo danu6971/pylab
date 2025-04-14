bb30ba2721799b70a7c285d5324c45dca802672d
danu6971 <imdanu6971@gmail.com>
2025-04-14 13:14:47 +0530

    Add files via upload

---
+++ Lab3/lab3q4.py
@@ -0,0 +1,4 @@
+squares = [num**2 for num in range(1, 21) if num % 2 == 0]
+sum_of_squares = sum(squares)
+print("List of squares of even numbers:", squares)
+print("Sum of all squares:", sum_of_squares)
